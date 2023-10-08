ae0126c8932b788b1b306087b7877a6ef6a78028
jcvieiradev <jcvieiradev@gmail.com>
2023-10-08 16:57:59 -0300

    aula4

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from '@mui/material';
 import React from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='/pagina-inicial' element={<p>Página inicial</p>} />
+            <Route path='/pagina-inicial' element={<Button>Botão</Button>} />
 
             <Route path='*' element={<Navigate to='/pagina-inicial' />} />
         </Routes>
