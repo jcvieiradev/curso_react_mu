863857d8ede52be8f060dd5f7824c62a6eadd922
jcvieiradev <jcvieiradev@gmail.com>
2023-10-15 18:00:47 -0300

    aula 14

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,19 @@
+import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import React from 'react';
 
 export const Dashboard = () => {
 
     return (
-        <LayoutBaseDePagina titulo='Página inicial'>
+        <LayoutBaseDePagina 
+            titulo='Página inicial'
+            barraDeFerramentas={(
+                <FerramentasDaListagem 
+                    mostrarInputBusca
+                    textoBotaoNovo='Nova'
+                />)}>
             Testando
+   
         </LayoutBaseDePagina>
     );
 };
